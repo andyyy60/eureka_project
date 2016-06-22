37843bcfad54c34b88b8c51025b1daa63ade9ca1
andyyy60 <andyyy60@gmail.com>
2016-06-22 13:40:36 -0700

    Fixed 60 second sequence bug

--- initial_tasks/task2.py
+++ initial_tasks/task2.py
@@ -2,39 +2,50 @@
 #todo: edge cases,empty lists
 #todo: cleanup with fresh mind
 
-import os, exifread, time
+import os, exifread, time, operator
 
 def exif_info2time(ts):
-	#changes EXIF date to number of seconds since 1970-01-01
+	#changes EXIF date to number of seconds since epoch
 	tpl = time.strptime(ts + 'UTC', '%Y:%m:%d %H:%M:%S%Z')
 	return time.mktime(tpl)
 
 #finds images in "sample_images" folder
 image_dir = os.getcwd()+"/sample_images/"
 
-timestamp = []
+timestamp = {}
 for filename in os.listdir(image_dir):
     f = open(image_dir + filename, 'rb')
     tags = exifread.process_file(f)
     for tag in tags.keys():
         if tag not in ('JPEGThumbnail', 'TIFFThumbnail'):  # these are not printable
             dt_value = '%s' % tags['Image DateTime']
-            timestamp.append(exif_info2time(dt_value))
+            timestamp[str(image_dir+filename)] = exif_info2time(dt_value)
 
-def assign_clusters(timestamp):
+sorted_timestamps = sorted(timestamp.items(), key=operator.itemgetter(1)) # create sorted list of (filename, time) tuples
+
+for n in range(0,len(sorted_timestamps)):
+    print (sorted_timestamps[0][1]-sorted_timestamps[n][1])
+
+def assign_clusters(timestamps):
     edge_indexes = []
-    for n in range(1, len(timestamp)):
-        if((timestamp[n]-timestamp[0])>60):
+    current = 0;
+    for n in range(1, len(sorted_timestamps)):
+        if(abs(sorted_timestamps[n][1]-sorted_timestamps[current][1])>60):
             edge_indexes.append(n)
-    clusters = [[] for i in range(len(edge_indexes))]
-    print(len(clusters))
+            current = n
+    clusters = [[] for i in range(len(edge_indexes)+1)]
+    print((clusters))
     current_index = 0
     current_cluster = 0
     for elements in range(len(edge_indexes)):
         for times in range(current_index, edge_indexes[elements]):
-            clusters[current_cluster].append(timestamp[times])
+            clusters[current_cluster].append(sorted_timestamps[times][1])
             current_index = times+1
         current_cluster+=1
+
+    for i in range(current_index, len(sorted_timestamps)):
+        clusters[current_cluster].append(sorted_timestamps[i][1])
+
 
     for item in range(len(clusters)):
         for j in (clusters[item]):
