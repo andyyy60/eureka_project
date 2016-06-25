06cfee5a0e85ff49e3aa7e652e3a4a8f0b992ea5
andyyy60 <andyyy60@gmail.com>
2016-06-24 18:47:05 -0700

    updated crop.py

--- initial_tasks/task3/crop.py
+++ initial_tasks/task3/crop.py
@@ -1,4 +1,5 @@
-import Image
+from PIL import Image
+import os
 
 def crop_image(input_image, output_image, start_x, start_y, width, height):
     #temperature is at crop_image("img.JPG","output", 1750, 0, 150, 40)
@@ -7,8 +8,30 @@ def crop_image(input_image, output_image, start_x, start_y, width, height):
     output_img = input_img.crop(box)
     output_img.save(output_image +".JPG")
 
+def gen_grid():
 
-def main():
-    crop_image("img.JPG","output", 1750, 0, 150, 40)
+    for image in os.listdir(os.getcwd()+"/train_digits/"):
+    # opens an image:
+        im = Image.open(os.getcwd()+"/train_digits/"+str(image))
+        # creates a new empty image, RGB mode, and size 400 by 400.
 
-if __name__ == '__main__': main()
+        # Iterate through a 4 by 4 grid with 100 spacing, to place my image
+        for i in xrange(0, 500, 100):
+            for j in xrange(0, 500, 100):
+                # I change brightness of the images, just to emphasise they are unique copies.
+                new_im = Image.eval(im, lambda x: x + (i + j) / 30)
+                # paste the image at location i,j:
+                new_im.paste(im, (i, j))
+
+    new_im.save("test.png")
+
+
+
+
+
+crop_image(os.getcwd() + "/sample_images/Main_2015-01-05_12_34_35_343.JPG", "7", 1755, 0, 38, 30)
+#crop_image(os.getcwd()+"/sample_images/Main_2015-01-18_08_52_23_114.JPG","output", 1750, 0, 80, 30) -->  both digits
+#crop_image(os.getcwd() + "/sample_images/Main_2015-01-18_08_52_23_114.JPG", "output", 1755, 0, 38, 30) -->  left digit
+#crop_image(os.getcwd() + "/sample_images/Main_2015-01-18_08_52_23_114.JPG", "output", 1755 + 35, 0, 38, 30) --> right didit
+
+
