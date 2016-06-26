59ecbebd5b0167de37b5b528736b5ba0b5b1495a
andyyy60 <andyyy60@gmail.com>
2016-06-25 19:10:46 -0700

    now trains by looping through 38x30 images in a directory

--- initial_tasks/task3/training_experiment.py
+++ initial_tasks/task3/training_experiment.py
@@ -1,44 +1,44 @@
 
-import sys
+import sys, os
 import numpy as np
 import cv2
 
-im = cv2.imread('no_space_stitch.png')
-im3 = im.copy()
 
-gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(gray, (5, 5), 0)
-thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
 
-#################      Now finding Contours         ###################
-
-image, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+x,y,w,h = 0, 0, 38, 30
 
 samples = np.empty((0, 100), np.float32)
 responses = []
 keys = [i for i in range(48, 58)]
 
-x,y,w,h = 0, 0, 38, 30
 
-for row in range(4):
-    for col in range(10):
-        cv2.rectangle(im, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        roi = thresh[y:y + h, x:x + w]
-        roismall = cv2.resize(roi, (10, 10))
-        cv2.imshow('norm', im)
-        key = cv2.waitKey(0)
-        key = 48+(key-1114032)
+for image in os.listdir(os.getcwd()+'/train_digits/'):
+    print image
+    im = cv2.imread('train_digits/'+image)
+    im3 = im.copy()
 
-        if key == chr(27).encode():  # (escape to quit)
-            sys.exit()
-        elif key in keys:
-            responses.append(int(chr(key)))
-            sample = roismall.reshape((1, 100))
-            samples = np.append(samples, sample, 0)
-        x +=38
-    x = 0
-    w = 38
-    y+=30
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
+
+    #################      Now finding Contours         ###################
+
+    image, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    cv2.rectangle(im, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    roi = thresh[y:y + h, x:x + w]
+    roismall = cv2.resize(roi, (10, 10))
+    cv2.imshow('norm', im)
+    key = cv2.waitKey(0)
+    key = 48+(key-1114032)
+
+    if key == chr(27).encode():  # (escape to quit)
+        sys.exit()
+    elif key in keys:
+        responses.append(int(chr(key)))
+        sample = roismall.reshape((1, 100))
+        samples = np.append(samples, sample, 0)
+
 
 
 responses = np.array(responses, np.float32)
