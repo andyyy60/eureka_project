f83574f975c908f74e925a1c31d81d672d0c73e3
andyyy60 <andyyy60@gmail.com>
2016-06-26 14:03:43 -0700

    added arguments and descriptions of functions stitch_horizontally and stitch_vertically

--- initial_tasks/task3/stitch.py
+++ initial_tasks/task3/stitch.py
@@ -2,37 +2,37 @@ import sys
 from PIL import Image
 
 
-images = map(Image.open, ['0.JPG','1.JPG','2.JPG','3.JPG','4.JPG','5.JPG','6.JPG','7.JPG','8.JPG','9.JPG'])
-widths, heights = zip(*(i.size for i in images))
+def stitch_horizontally(image_list):
+    '''Stitches together a list of images for data training purposes'''
+    images = map(Image.open, image_list)
+    widths, heights = zip(*(i.size for i in images))
 
-total_width = sum(widths)
-max_height = max(heights)
+    total_width = sum(widths)
+    max_height = max(heights)
 
-new_im = Image.new('RGB', (total_width, max_height))
+    new_im = Image.new('RGB', (total_width, max_height))
 
-x_offset = 0
-for im in images:
-  new_im.paste(im, (x_offset,0))
-  x_offset += im.size[0]
+    x_offset = 0
+    for im in images:
+        new_im.paste(im, (x_offset,0))
+        x_offset += im.size[0]
 
-new_im.save('no_space_stitch.jpg')
+    new_im.save('vertical_stitch.jpg')
 
-################STITCH VERTICALLY######################
+def stitch_vertically(image_list):
+    '''Stitches together a list of images for data training purposes, intended to
+    be used combined with stitch_horizontally'''
+    images = map(Image.open, image_list)
+    heights,widths = zip(*(i.size for i in images))
 
-# import sys
-# from PIL import Image
-#
-# images = map(Image.open, ['test.png','test.png','test.png','test.png'])
-# heights,widths = zip(*(i.size for i in images))
-#
-# total_width = sum(widths)
-# max_height = max(heights)
-#
-# new_im = Image.new('RGB', (max_height,total_width))
-#
-# x_offset =0
-# for im in images:
-#   new_im.paste(im, (0,x_offset))
-#   x_offset += im.size[1]
-#
-# new_im.save('no_space_stitch.png')
+    total_width = sum(widths)
+    max_height = max(heights)
+
+    new_im = Image.new('RGB', (max_height,total_width))
+
+    x_offset =0
+    for im in images:
+        new_im.paste(im, (0,x_offset))
+        x_offset += im.size[1]
+
+    new_im.save('horizontal_stitch.png')
