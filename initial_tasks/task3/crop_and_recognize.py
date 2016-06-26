76da4153663cb46dde7bd0a6992a2cbe2e8022fc
andyyy60 <andyyy60@gmail.com>
2016-06-26 13:23:42 -0700

    working crop and recognize with positive digits (<100)

--- initial_tasks/task3/crop_and_recognize.py
+++ initial_tasks/task3/crop_and_recognize.py
@@ -1,6 +1,6 @@
 ''''Author: Andy Rosales Elias, EUREKA! 2016, Univeristy of California, Santa Barbara | andy00@umail.ucsb.edu'''
 
-import crop, ocr_experiment, os, chandra_ocr
+import crop, ocr_contour, os, chandra_ocr
 
 def run():
     directory = os.getcwd()+'/sample_images/'
@@ -10,12 +10,14 @@ def run():
         # Reads through the images in a directory and crops temperatures
         crop.crop_image(directory+image, "temp/right_digit", 1755 + 35, 0, 38, 30) #crops right digit
         crop.crop_image(directory+image, "temp/left_digit", 1755, 0, 38, 30) #crops left digit
-        right = ocr_experiment.recogize(os.getcwd()+'/temp/right_digit.jpg')
-        left = ocr_experiment.recogize(os.getcwd()+'/temp/left_digit.jpg')
+        right = ocr_contour.recognize(os.getcwd()+'/temp/right_digit.jpg')
+        left = ocr_contour.recognize(os.getcwd()+'/temp/left_digit.jpg')
+        chandra = chandra_ocr.main(2, directory+image)
         os.remove(os.getcwd()+'/temp/right_digit.jpg')
         os.remove(os.getcwd()+'/temp/left_digit.jpg')
-        temperature = left*10+right
-        print "The temperature is {0}".format(temperature), image
+        temperature = left+right
+        if str(temperature) not in chandra:
+            print "mismatch: {0} {1} {2}".format(temperature, chandra, image)
 
 
 run()
