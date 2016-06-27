0b3242231460cc8b49c66fc8e1c46ccdc1061126
andyyy60 <andyyy60@gmail.com>
2016-06-27 00:14:04 -0700

    added function for recognizing a single image

--- initial_tasks/task3/crop_and_recognize.py
+++ initial_tasks/task3/crop_and_recognize.py
@@ -3,7 +3,8 @@
 import crop, ocr_contour, os
 
 def run(images_path, training_path):
-
+    '''reads temperature of images in a directory'''
+    #TODO: make it only read .JPG and TIFF
     for image in os.listdir(images_path):
 
         # Reads through the images in a directory and crops temperatures
@@ -19,6 +20,17 @@ def run(images_path, training_path):
 
     print "Success."
 
-run('/home/andy/PycharmProjects/ocr/sample_images/','/home/andy/PycharmProjects/ocr/')
+def run_single(image, training_path):
+    """reads temperature for a single image"""
+    # Reads through the images in a directory and crops temperatures
+    if not os.path.exists(os.getcwd()+'/temp/'): #if temp folder doesnt exis, create one
+        os.makedirs(os.getcwd()+'/temp/')
+    crop.crop_image(image, "temp/digits", 1710, 0, 115, 30) #crops digits
+    temperature = ocr_contour.recognize(os.getcwd()+'/temp/digits.jpg', training_path) #recognize right digit
+    os.remove(os.getcwd()+'/temp/digits.jpg') #clean up temp dir
+    temp = ''
+    for digit in temperature:
+        temp += digit
+    print "temperature is: {0}".format(temp)
 
-
+    print "Success."
