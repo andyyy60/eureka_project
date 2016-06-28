25dbec080aa62f875ee2b058f9c6ec513b327d34
andyyy60 <andyyy60@gmail.com>
2016-06-27 18:47:59 -0700

    added camera 3 coordinates

--- initial_tasks/task3/crop.py
+++ initial_tasks/task3/crop.py
@@ -9,19 +9,22 @@ def crop_image(input_image, output_image, start_x, start_y, width, height):
     output_img.save(output_image +".jpg")
 
 
-def invert(image): #inverts color in an image
+def invert(image):
     pic = Image.open(image)
     inverted = Image.eval(pic, lambda (x): 255 - x)
     inverted.save(str(image))
 
 
-#crop_image(os.getcwd()+"/sample_images/BoneT_2014-06-04_17_56_53_1881.JPG","hunnid", 800, 2350, 100, 95)#<-- 100 F
-#crop_image(os.getcwd()+"/sample_images/BoneT_2014-06-05_16_06_03_2009.JPG","outpu", 795 + 35, 2350 + 15, 35, 55)
-#invert('outpu.jpg')
 
+
+
+                                            ##CAMERA 3##
+#crop_image(os.getcwd() + "/sample_images/BoneH_2015-12-08_06_10_04_7013.JPG", "output", 425, 0, 55, 30)
 
 
                                             ##CAMERA 1##
+#crop_image(os.getcwd()+"/sample_images/BoneT_2014-06-04_17_56_53_1881.JPG","hunnid", 800, 2350, 100, 95)#<-- 100 F
+#crop_image(os.getcwd()+"/sample_images/BoneT_2014-06-05_16_06_03_2009.JPG","outpu", 795 + 35, 2350 + 15, 35, 55)
 #crop_image(os.getcwd() + "/sample_images/BoneT_2014-06-05_16_19_08_2016.JPG", "train_digits2/8", 795+35, 2350+15, 35, 55)<--right
 #crop_image(os.getcwd()+"/sample_images/BoneT_2014-06-05_09_44_07_1911.JPG","output", 750, 2350, 115, 95)<--both
 #crop_image(os.getcwd() + "/sample_images/BoneT_2014-06-05_16_19_08_2016.JPG", "train_digits2/output", 795, 2350+15, 38, 55)<left
