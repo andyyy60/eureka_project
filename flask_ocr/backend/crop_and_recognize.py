a5ec330c8848d666581072bfe6e5519e3cf94fca
andy <andyyy60@gmail.com>
2016-09-12 23:07:17 -0700

    Updated web service  backend files

--- flask_ocr/backend/crop_and_recognize.py
+++ flask_ocr/backend/crop_and_recognize.py
@@ -1,13 +1,16 @@
 ''''Author: Andy Rosales Elias, EUREKA! 2016, Univeristy of California, Santa Barbara | andy00@umail.ucsb.edu'''
-import crop, ocr_contour, os, time, cv2, argparse, sys
+import crop, ocr_contour, os, time, cv2, argparse, sys, random, string
 import pyexifinfo as exif
 from PIL import Image
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+   return ''.join(random.choice(chars) for _ in range(size))
 
 def check(base):
     im = Image.open(base)
     width, height = im.size
-    widths = [3264,1920,3776]
-    heights = [2448,1080,2124]
+    widths = [3264,1920,3776, 2688]
+    heights = [2448,1080,2124, 1512]
     exif_json = exif.get_json(base)
     try:
         temp = exif_json[0]['MakerNotes:AmbientTemperatureFahrenheit']
@@ -15,90 +18,84 @@ def check(base):
     except:
         pass
     if (width not in widths) or (height not in heights): #If the image is not from one of the cameras
-        return [-9999, [width, height], exif_json]
+        return ["Temp is: {0}".format(-9999), [width, height], exif_json]
     elif (widths.index(width) != heights.index(height)): #if its not the right w x h combination
-        return [-9999, [width, height], exif_json]
+        return ["Temp is: {0}".format(-9999), [width, height], exif_json]
     try:
         model = exif_json[0]['EXIF:Model']
         if model == "SG565FV-8M": #HCO ScoutGuard (no temp)
-            return [-9999, [width, height], exif_json]
+            return ["Temp is: {0}".format(-9999), [width, height], exif_json]
     except:
         pass
     return [None, [width, height], exif_json]
 
 def run_c2(image, training_path):
-    '''reads temperature of images in a directory'''
+    '''Reads temperature values from HC500/HC600 Hyperfire cameras
+        Writes any found mismatches on task3/mismatches/ in a .txt file
+        w/ a randomly generated filename'''
     info = check(image)
     valid = check(image)[0]
-    if valid != None:
-        return valid
     width, height = info[1][0], info[1][1]
-    exif_json = info[2]
-    valid_heights = [3776, 1920, 2048] #pictype 3 heights
-    valid_widths = [2124, 1080, 1536] #pictype 3 widths
+    valid_widths = [1920, 2048]  # pictype 2 heights
+    valid_heights = [1080, 1536]  # pictype 2 widths
     if width not in valid_widths or height not in valid_heights:
-        return -9999
-    if not os.path.exists(os.getcwd()+'/temp/'): #if temp folder doesnt exis, create one
-        os.makedirs(os.getcwd()+'/temp/')
-    crop.crop_image(image, "temp/digits.jpg", 1710, 0, 115, 30) #crops digits
-    temperature = ocr_contour.recognize(os.getcwd()+'/temp/digits.jpg', training_path) #recognize right digit
-    os.remove(os.getcwd()+'/temp/digits.jpg') #clean up temp dir
-    temp = ''
+        return "Temp is: {0}".format(-9999)
+    if not os.path.exists(os.getcwd() + '/temp/'):  # if temp folder doesnt exis, create one
+        os.makedirs(os.getcwd() + '/temp/')
+    if height == valid_heights[0] and width == valid_widths[0]: #1920x2040
+        crop.crop_image(image, "temp/digits.jpg", 1710, 0, 115, 30)  # crops digits
+    elif height == valid_heights[1] and width == valid_widths[1]: #2048x1536
+        crop.crop_image(image, "temp/digits.jpg", 1850, 0, 105, 30)
+    temperature = ocr_contour.recognize(os.getcwd() + '/temp/digits.jpg', training_path)  # recognize right digit
+    # os.remove(os.getcwd()+'/temp/digits.jpg') #clean up temp dir
+    ocr_temp = ''
     for digit in temperature:
-        temp += digit
+        ocr_temp += digit
+    if valid != None:
+        exif_temp = int(valid)
+    ocr_temp = int(ocr_temp)
     try:
-        return int(temp)
+        if ocr_temp != exif_temp:
+            if not os.path.exists(os.getcwd() + '/mismatches/'):  # if temp folder doesnt exis, create one
+                os.makedirs(os.getcwd() + '/mismatches/')
+            f = open("mismatches/{0}.jpg".format(id_generator()), "w")
+            f.write("exif temp: {0}, ocr temp: {1}, filename: {2}".format(exif_temp,ocr_temp,image))
+        return int(ocr_temp)
     except:
-        return -9999
+        return "Temp is: {0}".format(-9999)
 
 def run_c1(image, training_path):
     '''reads temperature of images in a directory. CAMERA 1 ONLY'''
     info = check(image)
     valid = info[0]
-    if valid != None:
-        return valid
     width, height = info[1][0], info[1][1]
     exif_json = info[2]
     valid_heights = [2448] #pictype 3 heights
     valid_widths = [3264] #pictype 3 widths
     if width not in valid_widths or height not in valid_heights:
-        return -9999
+        return "Temp is: {0}".format(-9999)
+    if valid != None:
+        return valid
     if not os.path.exists(os.getcwd() + '/temp/'):  # if temp folder doesnt exis, create one
         os.makedirs(os.getcwd() + '/temp/')
-    crop.crop_image(image,'temp/digits.jpg',  800, 2350, 100, 95)
+    crop.crop_image(image,'temp/digits.jpg',  790, 2355, 375, 70)
     crop.invert(os.getcwd() + '/temp/digits.jpg')
-    digits = len(ocr_contour.recognize(os.getcwd() + '/temp/digits.jpg', training_path))
+    info = ocr_contour.recognize(os.getcwd() + '/temp/digits.jpg', training_path, True)
+    temp = []
+    for item in info:
+        if item[0] == '45':
+            x1,y1 = item[1][0], item[1][1]
+    for item in info:
+        if item[1][0] < x1:
+            temp.append((item[0], item[1][0]))
+    final = sorted(temp, key = lambda x: x[1])
     temperature = ''
-    if digits == 1:  # crop rightmost digit
-        crop.crop_image(image, "temp/1.jpg", 795 + 35, 2350 + 15, 35, 55)
-        crop.invert(os.getcwd() + '/temp/1.jpg')
-        temperature = ocr_contour.recognize(os.getcwd() + '/temp/1.jpg', training_path)
-        os.remove(os.getcwd() + '/temp/1.jpg')  # clean up temp dir
-    if digits == 2:#If there's two digits, crop them to get them in order
-        crop.crop_image(image, "temp/1.jpg", 795 + 35, 2350 + 15, 35, 55)
-        crop.crop_image(image, "temp/2.jpg", 795, 2350 + 15, 38, 55)
-        crop.invert(os.getcwd() + '/temp/1.jpg')
-        crop.invert(os.getcwd() + '/temp/2.jpg')
-        right = ocr_contour.recognize(os.getcwd() + '/temp/1.jpg', training_path)
-        left = ocr_contour.recognize(os.getcwd() + '/temp/2.jpg', training_path)
-        temperature = left[0] + right[0]
-        os.remove(os.getcwd() + '/temp/1.jpg')  # clean up temp dir
-        os.remove(os.getcwd() + '/temp/2.jpg')  # clean up temp dir
-    elif digits == 3:#Edge case, crop 3 numbers and ignore any other symbols
-        crop.crop_image(image, "temp/1.jpg", 795 + 35, 2350 + 15, 35, 55)
-        crop.crop_image(image, "temp/2.jpg", 795, 2350 + 15, 38, 55)
-        crop.crop_image(image, "temp/3.jpg", 800+64, 2365, 100-57, 55)
-        crop.invert(os.getcwd() + '/temp/1.jpg')
-        crop.invert(os.getcwd() + '/temp/2.jpg')
-        crop.invert(os.getcwd() + '/temp/3.jpg')
-        right = ocr_contour.recognize(os.getcwd() + '/temp/1.jpg', training_path)
-        left = ocr_contour.recognize(os.getcwd() + '/temp/2.jpg', training_path)
-        extra = ocr_contour.recognize(os.getcwd() + '/temp/3.jpg', training_path)
-        temperature = left[0]+extra[0] + right[0]
+    for i in final:
+        temperature += i[0]
     try:
         return int(temperature)
     except:
-        return -9999
+        return "Temp is: {0}".format(-9999)
 
 def run_c3(image, training_path):
     """reads temperature for a single image"""
@@ -112,27 +109,56 @@ def run_c3(image, training_path):
     valid_heights = [2124] #pictype 3 heights
     valid_widths = [3776] #pictype 3 widths
     if width not in valid_widths or height not in valid_heights:
-        return -9999
+        return "Temp is: {0}".format(-9999)
     if not os.path.exists(os.getcwd()+'/temp/'): #if temp folder doesnt exis, create one
         os.makedirs(os.getcwd()+'/temp/')
     crop.crop_image(image, "temp/digits.jpg", 435, 0, 70, 30) #crops digits
-    temperature = ocr_contour.recognize('temp/digits.jpg', training_path) #recognize right digit
+    temperature = ocr_contour.recognize('temp/digits.jpg', training_path) #recognize digit
     os.remove(os.getcwd()+'/temp/digits.jpg') #clean up temp dir
     temp = ''
-    for digit in temperature:
-        temp += digit
+    for i in temperature:
+        temp +=i
     try:
         return int(temp)
     except:
-        return -9999
+        return "Temp is: {0}".format(-9999)
 
+def run_c4(image, training_path):
+    """reads temperature for a single image"""
+    #FOR CAMERA 4 ONLY
+    info = check(image)
+    valid = info[0]
+    if valid != None:
+        return valid
+    width, height = info[1][0], info[1][1]
+    exif_json = info[2]
+    valid_heights = [1512] #pictype 4 heights
+    valid_widths = [2688] #pictype 4 widths
+    if width not in valid_widths or height not in valid_heights:
+        return "Temp is: {0}".format(-9999)
+    if not os.path.exists(os.getcwd()+'/temp/'): #if temp folder doesnt exis, create one
+        os.makedirs(os.getcwd()+'/temp/')
+    crop.crop_image(image, "temp/digits.jpg",  825, 1445, 145, 70)
+    temperature = ocr_contour.recognize('temp/digits.jpg', training_path, True) #recognize digits
+    # im = cv2.imread('temp/digits.jpg') ##debugging
+    # cv2.imshow('img', im) ##debugging
+    # cv2.waitKey(0) ##debugging
+    final = sorted(temperature, key = lambda x: x[1][0]) #sort from left to right
+    os.remove(os.getcwd()+'/temp/digits.jpg') #clean up temp dir
+    temp = ''
+    for digit in final:
+        temp += digit[0]
+    try:
+        return int(temp)
+    except:
+        return "Temp is: {0}".format(-9999)
 
 def loop(type, path, debug = False):
     """Select camera 1,2 or 3"""
-    if type == 3:
+    if type == 4:
         for image in os.listdir(path):
-            temp = run_c3(path+image, 'data/data_files/camera_3/')
-            print "Temp is: {0}".format(temp)
+            temp = run_c4(path+image, 'data/data_files/camera_1/')
+            print "Temp is: {0}".format(temp), image
     if type == 2:
         for image in os.listdir(path):
             temp = run_c2(path+image, 'data/data_files/camera_2/')
@@ -150,30 +176,38 @@ def loop(type, path, debug = False):
             print "Temp is: {0}".format(temp)
 
 
-def main(pictype, base):
-    # parser = argparse.ArgumentParser(description='OCR Recognition tool for Sedgwick Reserve photos')
-    # parser.add_argument('pictype', action='store', type=int, help='1,2,3')
-    # parser.add_argument('base', action='store', help='JPEG image')
-    # # optional arguments
-    # # parser.add_argument('--blur','-b',action='store',default=False,type=bool,help='day threshold')
-    # args = parser.parse_args()
-    # if args.pictype < -1 and args.pictype > 3:
-    #     # 0 is hidden and used for testing
-    #     print 'pictype must be 1,2, or 3'
-    #     sys.exit(1)
+def main():
+    parser = argparse.ArgumentParser(description='OCR Recognition tool for Sedgwick Reserve photos')
+    parser.add_argument('pictype', action='store', type=int, help='1,2,3')
+    parser.add_argument('base', action='store', help='JPEG image')
+    # optional arguments
+    # parser.add_argument('--blur','-b',action='store',default=False,type=bool,help='day threshold')
+    args = parser.parse_args()
+    if args.pictype < -1 and args.pictype > 3:
+        # 0 is hidden and used for testing
+        print 'pictype must be 1,2, or 3'
+        sys.exit(1)
+    if str(args.base[-4:]).lower() != '.jpg':
+        return "Temp is: {0}".format(-9999)
+    try:
+        if args.pictype == 3:
+            temp = run_c3(args.base, 'data/data_files/camera_3/')
+            return "Temp is: {0}".format(temp)
+        if args.pictype == 2:
+            temp = run_c2(args.base, 'data/data_files/camera_2/')
+            return "Temp is: {0}".format(temp)
+        if args.pictype == 1:
+            temp = run_c1(args.base, 'data/data_files/camera_1/')
+            return "Temp is: {0}".format(temp)
+        if args.pictype == 4:
+            temp = run_c4(args.base, 'data/data_files/camera_1/')
+            return "Temp is: {0}".format(temp)
+    except:
+        return "Temp is: {0}".format(-9999)
 
-    if str(base[-4:]).lower() != '.jpg':
-        return -9999
-    if pictype == 3:
-        temp = run_c3(base, 'backend/data/data_files/camera_3/')
-        return int(temp)
-    if pictype == 2:
-        temp = run_c2(base, 'backend/data/data_files/camera_2/')
-        return int(temp)
-    if pictype == 1:
-        temp = run_c1(base, 'backend/data/data_files/camera_1/')
-        return int(temp)
 
-# crop.crop_image('/home/andy/Downloads/BoneH_2015-08-27_16-03-05_7659.JPG', "digits", 435, 0, 70, 30)  # crops digits
 
-# print main(3,'/home/andy/Downloads/BoneH_2015-08-27_09-27-31_7589.JPG')
+######################################
+if __name__ == "__main__":
+    print main()
+######################################
--- flask_ocr/backend/ocr_contour.py
+++ flask_ocr/backend/ocr_contour.py
@@ -2,7 +2,16 @@
 import cv2, imutils
 import numpy as np
 
-def recognize(images_path, training_path):
+def recognize(images_path, training_path, pass_info = False):
+    if "camera_1" in training_path:
+        threshhold =75
+        area = 110
+    elif "camera_2" in training_path:
+        threshhold =151
+        area = 70
+    elif "camera_3" in training_path:
+        threshhold = 151
+        area = 70
     #######   training part    ###############
     samples = np.loadtxt(training_path+'generalsamples.data', np.float32)
     responses = np.loadtxt(training_path+'generalresponses.data', np.float32)
@@ -17,14 +26,17 @@ def recognize(images_path, training_path):
     out = np.zeros(image.shape, np.uint8)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-    thresh = cv2.threshold(blurred, 151, 255, cv2.THRESH_BINARY)[1]#dont change 151
+    thresh = cv2.threshold(blurred, threshhold, 255, cv2.THRESH_BINARY)[1]#dont change 151
 
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
         cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+    # new = cv2.drawContours(image, cnts, -1, (0, 255, 0), 3)
+    # cv2.imshow('im', new)
+    # cv2.imshow('out', out)
+    # cv2.waitKey(0)
     content = []
     for c in cnts:
-
         # compute the center of the contour
         M = cv2.moments(c)
         cX = int(M["m10"] / M["m00"])
@@ -33,7 +45,7 @@ def recognize(images_path, training_path):
         # draw the contour and center of the shape on the image
         cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
         [x, y, w, h] = cv2.boundingRect(c)
-        if cv2.contourArea(c)>70: #the higher the threshold, the smaller the area
+        if cv2.contourArea(c)>area: #the higher the threshold, the smaller the area
             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
             roi = thresh[y:y + h, x:x + w]
             roismall = cv2.resize(roi, (10, 10))
@@ -41,10 +53,11 @@ def recognize(images_path, training_path):
             roismall = np.float32(roismall)
             retval, results, neigh_resp, dists = model.findNearest(roismall, k=1)
             string = str(int((results[0][0])))
-            if string != str(42):
-                cv2.putText(out, string, (x, y + h), 0, 1, (0, 255, 0))
-                content.append(string)
+            if pass_info: #returns temp between C and F symbols (camera 1)
+                content.append((string, [cX,cY]))
+            else:
+                if string != str(42):
+                    cv2.putText(out, string, (x, y + h), 0, 1, (0, 255, 0))
+                    content.append(string)
     content.reverse()
     return content
-
-
